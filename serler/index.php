fddbadf247629eda6405c7a67cf82fd294fe1823
jmssmith047 <jmssmith047@gmail.com>
2015-05-15 21:34:46 +1200

    Refactored Code to move all header code from all classes into one header.php file

--- serler/index.php
+++ serler/index.php
@@ -16,30 +16,7 @@
 </head>
 
 <body>
-	<div id=login>
-		<?php if($userlogin != ""){ echo "{$userlogin} <a href=\"logout.php\">Log Out</a>";} else echo "<a href=\"login.php\">Login to SERLER</a>"; ?>
-	</div>
-	<div id=quicksearch>
-	<form id="quicksearch" action = "searchstatusprocess.php" method = "get">
-		<input type="text" value="Search for Papers" 
-			onblur="if(this.value == '') { 
-			this.value = 'Search for Papers'; }" 
-			onfocus="if(this.value == 'Search for Papers') {
-			 this.value = ''; 
-			}" 
-		name="input">
-		<input type="Submit" name="button">
-	</form>
-	</div>
-
-	<h1>SERLER Software Engineering Research Laboratory Evidence Repository</h1>
-	
-	<ul class="menu">
-	<li><a href="index.php">Home</a></li>
-	<li><a href="poststatusform.php">Add a Paper</a></li>
-	<li><a href="searchstatusform.php">Search for a Paper</a></li>
-	<li><a href="about.php">About SERL</a></li>
-	</ul>
+	<?php require_once('header.php'); ?>
 
 
 
